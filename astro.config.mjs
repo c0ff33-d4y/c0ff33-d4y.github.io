0fb2884709bdb25a81e058a864567424802bb7c7
c0ff33-d4y <caodangchuong@hotmail.com>
2025-02-04 09:30:00 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://c0ff33-d4y.github.io',
+	base: "/",
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
